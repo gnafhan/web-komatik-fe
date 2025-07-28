a14ca89b1dbeb3395bcc6fcc29bc1d3cf630815e
Harfi <harfiibadurrahman@gmail.com>
2025-07-28 15:10:40 +0700

    feat: Build Gallery Kegiatan Section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,6 +3,7 @@ import { UiImage } from "@/components/ui/image";
 import HeroSection from "@/features/home/components/HeroSection";
 import PrestasiKami from "@/features/home/components/PrestasiKami";
 import TentangKami from "@/features/home/components/TentangKami";
+import GalleryKegiatan from "@/features/home/components/GalleryKegiatan";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <HeroSection />
       <TentangKami />
       <PrestasiKami />
+      <GalleryKegiatan />
     </div>
   );
 }
